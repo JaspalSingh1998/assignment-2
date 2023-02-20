8b49d3f042c823ad2eec0afb76ffbf13edef7d9c
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-02-19 22:07:29 -0500

    Added React-Confetti on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import ReactConfetti from 'react-confetti';
 
 const Home = () => {
   return (
-    <div className='mt-24'>Home</div>
+    <div className='grid place-items-center min-h-screen'>
+      <div className='text-center'>
+        <h1 className='text-8xl mb-8 font-bold'>Hey I'm Jaspal Singh</h1>
+        <p className='text-4xl'>I'm a web developer based out of India.</p>
+      </div>
+      <ReactConfetti />
+    </div>
   )
 }
 
